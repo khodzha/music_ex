ba70fd99061ad82905a4702fede7cdb584d049b8
Shamir Khodzha <khodzha.sh@gmail.com>
2017-09-08 23:21:24 +0300

    raise on guild supervisor start error

--- apps/music_ex_discord/lib/music_ex_discord/supervisor.ex
+++ apps/music_ex_discord/lib/music_ex_discord/supervisor.ex
@@ -7,7 +7,8 @@ defmodule MusicExDiscord.Supervisor do
 
   def start_guild_supervisor(guild) do
     spec = MusicExDiscord.GuildSupervisor.child_spec([guild])
-    Supervisor.start_child(__MODULE__, spec)
+    tuple = Supervisor.start_child(__MODULE__, spec)
+    :ok = element(1, tuple)
   end
 
   def init(:ok) do
